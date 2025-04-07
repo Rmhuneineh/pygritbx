b2883edb2f10f904dd981450b14aa62125bf1b42
Ragheed Huneineh <ragheedmhuneineh@outlook.com>
2025-04-07 15:16:41 +0300

    Added description of class members as part of the documentation

--- Component.py
+++ Component.py
@@ -2,6 +2,13 @@
 This is the "Component" class.
 It's a parent class that defines general properties that are common among different components.
 Other classes will inherit these properties instead of having to define every single time.
+Properties are:
+1) "name": a string of characters acting as a label
+2) "material": a "Material" object of the material class defining the material properties of the component
+3) "axis": a 3-element vector representing the axis along which the component is rotating with respect to a defined reference frame
+4) "loc": a 3-element vector representing the location of the component with respect to a defined reference frame
+5) "F_tot": the total force acting on the component
+6) "T_tot": the total torque acting on the component
 '''
 class Component:
 
